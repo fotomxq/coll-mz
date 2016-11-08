66d2e969bcc6f3bfabd5fcf6357c26cca6a3d10b
fotomxq <fotomxq@gmail.com>
2016-11-09 00:29:32 +0800

    完善构架，由于发现通用构架存在各种问题，所以改为各类细化的子方法，外部构建新的函数直接调用相关方法实现采集功能。

--- getPageData.go
+++ getPageData.go
@@ -1,44 +1,17 @@
 //获取任意站点整站分页数据
 //可复用，获取任意站点的，某一类页面的所有页面内容
-//注意，某些极特殊页面无法使用该模块，使用前需要给定参数测试一下
+//注意，某些非通用页面无法使用该模块run功能，但可以使用模块内其他方法辅助实现采集目标
 package main
 
 import (
 	"database/sql"
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/fotomxq/ftmp-libs"
+	"github.com/PuerkitoBio/goquery"
 )
 
 //结构
 type GetPageData struct {
-	//页码页面的URL前缀部分
-	pageURLPrefix string
-	//后缀部分
-	pageURLSuffix string
-	//在页码页面上查询出所有可选的子页面，jquery.find字符串形式
-	pageFindStr string
-	//以页码查询子节点为基准，查找标题
-	//如果该部分留空，则忽略
-	//必须确保childrenTitleFindStr有内容，否则数据无法采集
-	pageTitleFindStr string
-	//是否直接从该页面上采集数据
-	//true - 不递归查询子页面，直接将找出来的数据存档
-	//false - 继续递归查询子页面内容，根据指定jquery.find内容保存
-	isOnlyPage bool
-	//子页面查询内容，jquery.find字符串形式
-	childrenFindStr string
-	//子页面的标题，jquery.find字符串形式
-	childrenTitleFindStr string
-	//在页码中查询最大页数的查询路径，jquery.find形式
-	//注意，必须确保可通过该对象的jquery.html()获取数据
-	//如果留空，则从最开始一直读取到没法获取页面为止
-	pageMaxFindStr string
-	//从第几页开始
-	pageStart int
-	//第几页强行停止
-	pageEnd int
-	//开始和结束是否为正序
-	isPageSortAsc bool
 	//数据库的类型
 	dbType string
 	//数据库的DNS
@@ -47,6 +20,8 @@ type GetPageData struct {
 	log ftmplibs.Log
 	//数据库对象
 	db *sql.DB
+	//数据库连接失败error
+	dbErr error
 }
 
 //建立后初始化该结构
@@ -60,8 +35,15 @@ func (getPageData *GetPageData) Create(log ftmplibs.Log,configData map[string]in
 }
 
 //开始运行采集
+//如果该方法无法使用，请直接构建新的方法，并选择性使用模块内其他方法
 func (getPageData *GetPageData) Run() (bool,error){
-	defer db.Close()
+	//连接数据库
+	if getPageData.dbErr != nil{
+		getPageData.log.AddLog("发生一个错误:")
+		getPageData.log.AddErrorLog(err)
+	}
+	defer getPageData.db.Close()
+	//返回成功
 	return true,nil
 }
 
@@ -71,11 +53,155 @@ func (getPageData *GetPageData) check() (bool,error){
 }
 
 //连接数据库
-func (getPageData *GetPageData) connectDB() error{
-	getPageData.db, err = sql.Open(config.Data["databaseType"].(string),config.Data["databaseDNS"].(string))
-	if err != nil{
-		getPageData.log.AddLog("发生一个错误:")
-		getPageData.log.AddErrorLog(err)
+func (getPageData *GetPageData) ConnectDB(){
+	getPageData.db, getPageData.dbErr = sql.Open(config.Data["databaseType"].(string),config.Data["databaseDNS"].(string))
+}
+
+//获取页面所有子页面链接
+func (getPageData *GetPageData) GetPageList(url string,search string)([][]string,error){
+	//定义返回清单
+	var list [][]string
+	//获取页面
+	doc, err := goquery.NewDocument(url)
+	if err != nil {
+		return list,err
 	}
-	return err
+	node := doc.Find(search)
+	//遍历所有子链接
+	for i := range node.Nodes {
+		//获取节点数据
+		thisNode := node.Eq(i).Children()
+		thisHref, hrefExist := thisNode.Attr("href")
+		thisTitile,titleExist := thisNode.Attr("title")
+		if hrefExist == false || titleExist == false {
+			continue
+		}
+		//检查数据库中，是否存在该数据
+		checkBool,checkErr := getPageData.CheckRepeat(thisHref,thisTitile)
+		if checkErr != nil{
+			return list,checkErr
+		}
+		if checkBool == true{
+			continue
+		}
+		//如果该链接标题不存在，将链接和标题加入到组
+		child := []string{thisHref,thisTitile}
+		append(list,child)
+	}
+	return list,nil
+}
+
+//保存某个子页面
+func (getPageData *GetPageData) SavePageChildren(url string,title string,search string)(bool,error){
+	//读取页面数据
+	doc, docErr := goquery.NewDocument(url)
+	if docErr != nil {
+		return false,docErr
+	}
+	//检查是否存在对象
+	node := doc.Find(search)
+	for i := range node.Nodes {
+
+	}
+	return true,nil
+}
+
+//检查数据是否已采集
+func (getPageData *GetPageData) CheckRepeat(url string,title string)(bool,error){
+	//检查是否连接到数据库
+	if getPageData.dbErr != nil{
+		return false,getPageData.dbErr
+	}
+	//构建查询语句
+	var query string = "select id from `loadurl` where `url` = ? and `title` = ?"
+	//开始查询
+	stmt,stmtErr := getPageData.db.Prepare(query)
+	if stmtErr != nil{
+		return false,stmtErr
+	}
+	rows,resErr := stmt.Query(url,title)
+	if resErr != nil{
+		return false,resErr
+	}
+	rows.Next()
+	var id int
+	scanErr := rows.Scan(&id)
+	if scanErr != nil{
+		return false,nil
+	}
+	if id > 0{
+		return true,nil
+	}
+	//返回
+	return false,nil
+}
+
+//向数据库添加新的数据
+func (getPageData *GetPageData) AddNewData(url string,title string) (bool,error){
+	//检查是否连接到数据库
+	if getPageData.dbErr != nil{
+		return false,getPageData.dbErr
+	}
+	//构建添加语句
+	var query string = "insert into `loadurl`(`url`,`title`) values(?,?)"
+	stmt,stmtErr := getPageData.db.Prepare(query)
+	if stmtErr != nil{
+		return false,stmtErr
+	}
+	res,resErr := stmt.Exec(url,title)
+	if resErr != nil {
+		return false,resErr
+	}
+	newID,newIDErr := res.LastInsertId()
+	if newIDErr != nil {
+		return false,newIDErr
+	}
+	if newID > 0{
+		return true,nil
+	}
+	return false,nil
+}
+
+//向数据库添加视频记录
+func (getPageData *GetPageData) AddVideoData(url string) (bool,error){
+	//检查是否连接到数据库
+	if getPageData.dbErr != nil{
+		return false,getPageData.dbErr
+	}
+	var query string = "insert into `video`(`url`) values(?)"
+	stmt,stmtErr := getPageData.db.Prepare(query)
+	if stmtErr != nil{
+		return false,stmtErr
+	}
+	res,resErr := stmt.Exec(url)
+	if resErr != nil {
+		return false,resErr
+	}
+	newID,newIDErr := res.LastInsertId()
+	if newIDErr != nil {
+		return false,newIDErr
+	}
+	if newID > 0{
+		return true,nil
+	}
+	return false,nil
+}
+
+//将页面存到文件
+//该方法可用于保存图像等数据
+func (getPageData *GetPageData) SaveUrlToFile(url string,src string) (bool,error){
+	http := new(ftmplibs.SimpleHttp)
+	http.SetSendUrl(url)
+	c,err := http.Get()
+	if err != nil{
+		return false,err
+	}
+	t,tErr := ftmplibs.WriteFile(src,c)
+	if tErr != nil{
+		return false,nil
+	}
+	if t != false{
+		return false,nil
+	}
+	return true,nil
 }
